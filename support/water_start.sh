96305fad300043ea4ad843ac8eac74e50564cbf9
Alvaro Prieto <source@alvaroprieto.com>
2019-12-28 19:27:52 -0800

    Add pump start/stop code to work better with schedule/loggin

--- support/water_start.sh
+++ support/water_start.sh
@@ -3,5 +3,5 @@
 # Make sure we're in the right directory
 cd `git rev-parse --show-toplevel`
 
-python3 water.py /home/pi/config.yml --sleep 5
+python3 -u water.py /home/pi/config.yml --sleep 1
 
--- water.py
+++ water.py
@@ -36,22 +36,49 @@ def load_config():
         raise Exception("Unable to load config file")
 
 
+print("Loading initial configuration")
 load_config()
+
+pumps = []
 
 if on_pi:
     gpio.setmode(gpio.BCM)
+    print("Enumerating pumps")
     for pump in config["pumps"]:
+        print(pump)
         gpio.setup(pump, gpio.OUT, initial=1)
+        pumps.append(False)
+
 
 while True:
     load_config()
 
+    old_pump_values = pumps
+
+    # Assume all pumps are off unless determined otherwise
+    pumps = [False] * len(pumps)
+
     for item in config["schedule"]:
+        pump_no = item["pump_no"]
         if item["start_time"] < datetime.now() < item["stop_time"]:
+            pumps[pump_no] = True
+
+    for pump_no, enabled in enumerate(pumps):
+        if old_pump_values[pump_no] is False and enabled is True:
             print(
                 datetime.now(),
-                "Running Pump {}".format(item["pump_no"]),
-                config["pumps"][item["pump_no"]],
+                "Starting Pump {}".format(pump_no),
+                config["pumps"][pump_no],
             )
+            if on_pi:
+                gpio.output(config["pumps"][pump_no], 0)
+        elif old_pump_values[pump_no] is True and enabled is False:
+            print(
+                datetime.now(),
+                "Stopping Pump {}".format(pump_no),
+                config["pumps"][pump_no],
+            )
+            if on_pi:
+                gpio.output(config["pumps"][pump_no], 1)
 
     time.sleep(args.sleep)
